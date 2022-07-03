228622e6f7584998c83155e747d5d878f59408e7
vinagunawan <107608324+vinagunawan@users.noreply.github.com>
2022-07-03 21:38:45 +0800

    Add files via upload

---
+++ tabel_mahasiswa.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 03, 2022 at 02:55 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tabel mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datamahasiswa`
+--
+
+CREATE TABLE `datamahasiswa` (
+  `id` int(11) NOT NULL,
+  `Nama` varchar(225) NOT NULL,
+  `NIM` int(8) NOT NULL,
+  `Program_studi` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `datamahasiswa`
+--
+
+INSERT INTO `datamahasiswa` (`id`, `Nama`, `NIM`, `Program_studi`) VALUES
+(1, 'vina', 11111111, 'bb');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `datamahasiswa`
+--
+ALTER TABLE `datamahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `datamahasiswa`
+--
+ALTER TABLE `datamahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
